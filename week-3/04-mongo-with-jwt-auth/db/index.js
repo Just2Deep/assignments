5fac62f541ab79c7475fee1e122fad941081c218
Just2Deep <deepakrajsrs@gmail.com>
2023-12-23 12:59:03 +0530

    removed env file

--- week-3/04-mongo-with-jwt-auth/db/index.js
+++ week-3/04-mongo-with-jwt-auth/db/index.js
@@ -1,27 +1,76 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+const DB_NAME = "course";
+const response = mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
 
 // Define schemas
-const AdminSchema = new mongoose.Schema({
-    // Schema definition here
-});
+const AdminSchema = new mongoose.Schema(
+    {
+        // Schema definition here
+        username: {
+            type: String,
+            unique: true,
+        },
+        password: {
+            type: String,
+        },
+        token: {
+            type: String,
+        },
+    },
+    { timestamps: true }
+);
 
-const UserSchema = new mongoose.Schema({
-    // Schema definition here
-});
+const UserSchema = new mongoose.Schema(
+    {
+        // Schema definition here
+        username: {
+            type: String,
+            unique: true,
+        },
+        password: {
+            type: String,
+        },
+        token: {
+            type: String,
+        },
+        purchasedCourses: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Course",
+            },
+        ],
+    },
+    { timestamps: true }
+);
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title: {
+        type: String,
+    },
+    description: {
+        type: String,
+    },
+    price: {
+        type: Number,
+    },
+    imageLink: {
+        type: String,
+    },
+    published: {
+        type: Boolean,
+        default: false,
+    },
 });
 
-const Admin = mongoose.model('Admin', AdminSchema);
-const User = mongoose.model('User', UserSchema);
-const Course = mongoose.model('Course', CourseSchema);
+const Admin = mongoose.model("Admin", AdminSchema);
+const User = mongoose.model("User", UserSchema);
+const Course = mongoose.model("Course", CourseSchema);
 
 module.exports = {
     Admin,
     User,
-    Course
-}
+    Course,
+};
--- week-3/04-mongo-with-jwt-auth/routes/admin.js
+++ week-3/04-mongo-with-jwt-auth/routes/admin.js
@@ -3,20 +3,65 @@ const adminMiddleware = require("../middleware/admin");
 const router = Router();
 
 // Admin Routes
-app.post('/signup', (req, res) => {
+app.post("/signup", async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        if (!username || !password) {
+            return res
+                .status(400)
+                .json({ message: "All fields are required!" });
+        }
+
+        const admin = await Admin.findOne({ username });
+
+        if (admin) {
+            return res.status(409).json({ message: "user already exists!" });
+        }
+
+        const newAdmin = await Admin.create({ username, password });
+        if (!newAdmin) {
+            return res
+                .status(500)
+                .json({ message: "error while creating user!" });
+        }
+
+        return res.status(200).json({ message: "Admin created succcessfully" });
+    } catch (error) {
+        throw res.status(500).json({ message: "Internal Error" });
+    }
+});
+
+app.post("/signin", (req, res) => {
     // Implement admin signup logic
 });
 
-app.post('/signin', (req, res) => {
-    // Implement admin signup logic
+app.post("/courses", adminMiddleware, (req, res) => {
+    const { title, description, price, imageLink } = req.body;
+
+    if (!(title && description && price && imageLink)) {
+        return res.status(400).json({ message: "All fields are mandatory" });
+    }
+
+    const course = Course.create({
+        title,
+        description,
+        price,
+        imageLink,
+        published: true,
+    });
+
+    if (!course) {
+        return res
+            .status(500)
+            .json({ message: "some error while creating course" });
+    }
 });
 
-app.post('/courses', adminMiddleware, (req, res) => {
-    // Implement course creation logic
-});
+app.get("/courses", adminMiddleware, async (req, res) => {
+    const courses = await Course.find({ published: true });
 
-app.get('/courses', adminMiddleware, (req, res) => {
-    // Implement fetching all courses logic
+    return res.status(200).json({ courses });
 });
 
 module.exports = router;

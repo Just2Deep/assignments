676ce2bcadda1150604e3ea0a6b85153699e688c
Just2Deep <deepakrajsrs@gmail.com>
2024-01-15 14:48:15 +0530

    completed assignment 1 and 2

--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -1,27 +1,56 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo } from "react";
 // You have been given a list of items you shopped from the grocery store
 // You need to calculate the total amount of money you spent
 
 export const Assignment3 = () => {
+    const [name, setName] = useState("");
+    const [value, setValue] = useState(0);
     const [items, setItems] = useState([
-        { name: 'Chocolates', value: 10 },
-        { name: 'Chips', value: 20 },
-        { name: 'Onion', value: 30 },
-        { name: 'Tomato', value: 30 },
+        { name: "Chocolates", value: 10 },
+        { name: "Chips", value: 20 },
+        { name: "Onion", value: 30 },
+        { name: "Tomato", value: 30 },
         // Add more items as needed
     ]);
 
     // Your code starts here
-    const totalValue = 0;
+    const totalValue = useMemo(() => {
+        let total = 0;
+
+        for (let item of items) {
+            total += +item.value;
+        }
+
+        return total;
+    }, [items]);
     // Your code ends here
+
+    const addItem = () => {
+        setItems([...items, { name, value }]);
+    };
     return (
         <div>
             <ul>
                 {items.map((item, index) => (
-                    <li key={index}>{item.name} - Price: ${item.value}</li>
+                    <li key={index}>
+                        {item.name} - Price: ${item.value}
+                    </li>
                 ))}
             </ul>
             <p>Total Value: {totalValue}</p>
+            <label htmlFor="item1">Item</label>
+            <input
+                type="text"
+                id="item1"
+                onChange={(e) => setName(e.target.value)}
+            />
+            <label htmlFor="values">Value</label>
+            <input
+                type="number"
+                id="values"
+                onChange={(e) => setValue(e.target.value)}
+            />
+            <button onClick={addItem}>add item</button>
         </div>
     );
 };
--- week-6/2-use-callback/src/components/Assignment1.jsx
+++ week-6/2-use-callback/src/components/Assignment1.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 // Create a counter component with increment and decrement functions. Pass these functions to a child component which has buttons to perform the increment and decrement actions. Use useCallback to ensure that these functions are not recreated on every render.
 
@@ -6,22 +6,26 @@ export function Assignment1() {
     const [count, setCount] = useState(0);
 
     // Your code starts here
-    function handleIncrement() {
+    const handleIncrement = useCallback(() => {
+        setCount((count) => count + 1);
+    }, []);
 
-    }
+    const handleDecrement = useCallback(() => {
+        setCount((count) => count - 1);
+    }, []);
 
-    function handleDecrement() {
-        
-    }
     // Your code ends here
 
     return (
         <div>
             <p>Count: {count}</p>
-            <CounterButtons onIncrement={handleIncrement} onDecrement={handleDecrement} />
+            <CounterButtons
+                onIncrement={handleIncrement}
+                onDecrement={handleDecrement}
+            />
         </div>
     );
-};
+}
 
 const CounterButtons = ({ onIncrement, onDecrement }) => (
     <div>
